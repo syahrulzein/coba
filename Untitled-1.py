a5caf4fade2af028b7d00840d2dcbef8e0758bd9
syahrulzein <syahruls.zein@gmail.com>
2025-12-12 10:58:27 +0700

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-df = pd.read_csv('healthcare-dataset-stroke-data.csv')
+df = pd.read_excel('healthcare-dataset-stroke-data.xlsx')
 
 #1. Membaca Dataset
 st.write('**1. Dataset**')
